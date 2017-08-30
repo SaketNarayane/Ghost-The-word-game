56ead8258e0d568d752f9085100a60ccee0a8abc
Saket Narayane <snarayane@student.nitw.ac.in>
2017-08-31 04:25:29 +0530

    Add source files

---
+++ src/main/java/com/android/saket/ghost/GhostDictionary.java
@@ -0,0 +1,9 @@
+package com.android.saket.ghost;
+
+
+public interface GhostDictionary {
+    public final static int MIN_WORD_LENGTH = 4;
+    boolean isWord(String word);
+    String getAnyWordStartingWith(String prefix);
+    String getGoodWordStartingWith(String prefix);
+}
