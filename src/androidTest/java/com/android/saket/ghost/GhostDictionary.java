00997564263838d8d0a30cbfd2214076d2012a29
Saket Narayane <snarayane@student.nitw.ac.in>
2017-08-31 04:30:49 +0530

    Delete GhostDictionary.java

--- src/androidTest/java/com/android/saket/ghost/GhostDictionary.java
+++
@@ -1,9 +0,0 @@
-package com.android.saket.ghost;
-
-
-public interface GhostDictionary {
-    public final static int MIN_WORD_LENGTH = 4;
-    boolean isWord(String word);
-    String getAnyWordStartingWith(String prefix);
-    String getGoodWordStartingWith(String prefix);
-}
